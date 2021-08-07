0ca556c44c4f455cf87b50083e211da0df7584f2
mgracioli <michel-rg@hotmail.com>
2021-08-06 21:52:08 -0300

    criação do tabuleiro e states

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  //ESTILOS
+  const tabu={
+    display:'flex',
+    flexDirection:'column'  //estabelece o eixo principal do display
+  }
+
+  const tabuLinha={
+    display:'flex',
+    flexDirection:'row'
+  }
+
+  const casa={
+    width:100,
+    heigth:100,
+    display:'flex',
+    justifyContent:'enter',
+    alignItems:'center',
+    flexDirection:'row',
+    cursor:'pointer',
+    fontSize:60,
+    border:'1px solid #000'
+  }
+
+  //JS
+  const jogoInicial=[['','',''],['','',''],['','','']]
+  const [jogo,setJogo]=useState(jogoInicial)
+  const [simboloAtual,setSimboloAtual]=useState('X')
+  const [jogando,setJogando]=useState(true)
+
+  const tabuleiro = (j) => {
+    return(
+      <div style={tabu}>
+        <div style={tabuLinha}>
+          <div style={casa} data-pos='00' onClick="">{j[0][0]}</div>
+          <div style={casa} data-pos='01' onClick="">{j[0][1]}</div>
+          <div style={casa} data-pos='02' onClick="">{j[0][2]}</div>
+        </div>
+        <div style={tabuLinha}>
+          <div style={casa} data-pos='10' onClick="">{j[1][0]}</div>
+          <div style={casa} data-pos='11' onClick="">{j[1][1]}</div>
+          <div style={casa} data-pos='12' onClick="">{j[1][2]}</div>
+        </div>
+        <div style={tabuLinha}>
+          <div style={casa} data-pos='20' onClick="">{j[2][0]}</div>
+          <div style={casa} data-pos='21' onClick="">{j[2][1]}</div>
+          <div style={casa} data-pos='22' onClick="">{j[2][2]}</div>
+        </div>
+      </div>
+    )
+  }
+
+  const verificaVitoria=() => {
+    
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     {tabuleiro(jogo)}
+    </>
   );
 }
 
